184e40e6919599c8b38703f06a57920a2690d6e9
Mathieu Gagne <gagne.mathieu@hotmail.com>
2015-11-24 22:03:13 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'mechanize'
+gem 'terminal-table'
+gem 'dotenv'
---
+++ lib/berlin_ai_tournament_mode.rb
@@ -0,0 +1,29 @@
+module BerlinAITournamentMode
+
+  require 'rubygems'
+  require 'mechanize'
+  require 'logger'
+
+  class CreateGame
+
+    def initialize
+      # Create a new mechanize object
+      mech = Mechanize.new
+      mech.log = Logger.new $stderr
+      mech.agent.http.debug_output = $stderr
+
+      # Load the rubyforge website
+      page = mech.get('http://berlin-ai.com/games/new')
+      form = page.forms[1] # Select the first form
+      # form["session[who]"] = ARGV[0]
+      # form["session[password]"] = ARGV[1]
+      # form["commit"] = "Sign in"
+
+      # Submit the form
+      # page = form.submit form.buttons.first
+
+      puts form.body # Print out the body
+    end
+  end
+
+end
